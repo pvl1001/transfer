6adb8fdc45f461b5308ca14a6aee04b868b6e121
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2022-12-12 19:38:18 +0300

    alias '@'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig( {
+   plugins: [ react() ],
+   resolve: {
+      alias: {
+         '@': path.resolve( __dirname, './src' ),
+         '@icons': path.resolve( __dirname, './src/assets/icons' ),
+         '@styles': path.resolve( __dirname, './src/assets/styles' ),
+         '@var': path.resolve( __dirname, './src/assets/styles/variable.scss' ),
+      },
+   },
+} )
