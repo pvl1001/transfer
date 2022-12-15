ca7a4f8a6ddaf41a105d870e9d211a4a47e64d4f
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2022-12-15 22:31:10 +0300

    настройка vite.config.js
    + postcss
    + purgecss
    + autoprefixer
    + material ui

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import autoprefixer from "autoprefixer"
+import purgecss from "@fullhuman/postcss-purgecss"
+
 
 export default defineConfig( {
    plugins: [ react() ],
@@ -12,4 +15,32 @@ export default defineConfig( {
          '@var': path.resolve( __dirname, './src/assets/styles/variable.scss' ),
       },
    },
+   css: {
+      postcss: {
+         plugins: [
+            autoprefixer( {
+               "flexbox": "no-2009"
+            } ),
+            purgecss( { // убирает неиспользуемые css
+               content: [
+                  './src/pages/**/*.{js,jsx,ts,tsx}',
+                  './src/components/**/*.{js,jsx,ts,tsx}'
+               ],
+               defaultExtractor: content => content.match( /[\w-/:]+(?<!:)/g ) || [],
+               safelist: {
+                  standard: [
+                     "html",
+                     "body",
+                  ],
+                  deep: [ // вложенные селекторы (.tippy .modal)
+                     /_/,
+                  ],
+                  greedy: [ // зависимые селекторы (.tippy.modal)
+                     /_/,
+                  ]
+               }
+            } ),
+         ]
+      }
+   }
 } )
