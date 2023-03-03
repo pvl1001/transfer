9b545981d7fb722aa757efff81e5243e54512adf
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2023-03-03 12:38:42 +0300

    upd Button icon "Выгрузить заявки в .XLS"

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,18 +2,16 @@ import s from "./MainPage.module.scss"
 import MainHeader from "../../components/MainHeader/MainHeader";
 import TableContainer from "../../components/TableContainer/TableContainer";
 import { Button } from "@megafon/ui-core";
-// import Icon from '@megafon/ui-icons/basic-24-download_24.svg';
+import { ReactComponent as Icon } from '@megafon/ui-icons/basic-24-download_24.svg';
 
 
 function MainPage() {
-
    return (
       <div className={ s.MainPage }>
          <MainHeader/>
-         {/*<Icon/>*/}
          <div className={ `${ s.MainPage__container } wrapper` }>
             <TableContainer/>
-            <Button theme="purple" sizeAll="small" className={ s.MainPage__download_btn }>
+            <Button theme="purple" icon={ <Icon/> } className={ s.MainPage__download_btn }>
                Выгрузить заявки в .XLS
             </Button>
          </div>
